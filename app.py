10096a43a055c40e3901c393c4ec427f3c0f89ed
yeins-aristizabal-renault <yeins.aristizabal-extern@renault.com>
2025-06-04 14:36:05 -0500

    test 001

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.set_page_config(page_title="Hello World")
 
 st.title("👋 Hello, World!")
 st.subheader("Este é meu primeiro app com Streamlit")
-st.write("Se você está vendo isso, significa que o app está funcionando  ! 🎉")
+st.write("Se você está vendo isso, significa que o app está funcionando! 🎉")
