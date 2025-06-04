651f5933a12c793407f581b493e3a9ea2600ad02
yeins-aristizabal-renault <yeins.aristizabal-extern@renault.com>
2025-06-04 12:55:16 -0500

    Delete app/app.py

--- app/app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Hello World", layout="centered")
-
-st.title("👋 Hello, World!")
-st.subheader("Este é meu primeiro app com Streamlit")
-st.write("Se você está vendo isso, significa que o app está funcionando  ! 🎉")
